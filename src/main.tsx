0b28d6a2fdc73003292d6f578b7aa891e4e3a0d0
jimmyxuexue <1002661758@qq.com>
2024-02-24 15:18:43 +0800

    🎉 项目初始化

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,30 @@ import App from './App'
 import './index.css'
 
 import './demos/ipc'
+import { config } from './service/react-query'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { DwgConversion } from './page/dwgConversion'
 // If you want use Node.js, the`nodeIntegration` needs to be enabled in the Main process.
 // import './demos/node'
 
+const Root = () => {
+	const { queryClient, QueryClientProvider } = config()
+
+	return (
+		<React.StrictMode>
+			<QueryClientProvider client={queryClient}>
+				<Router>
+					<Routes>
+						<Route path="*" element={<App />} />
+					</Routes>
+				</Router>
+			</QueryClientProvider>
+		</React.StrictMode>
+	)
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<Root />
 )
 
 postMessage({ payload: 'removeLoading' }, '*')
---
+++ src/page/dwgConversion/index.tsx
@@ -0,0 +1,5 @@
+import { Button } from 'antd'
+
+export function DwgConversion() {
+	return <Button>上传</Button>
+}
